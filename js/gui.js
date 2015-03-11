5e8a881a2dc9e6b85bb79d8c3aecba9bb593cbba
Tony Boyles <tboyles@sosacorp.com>
2015-03-11 16:30:22 -0400

    Major Cleanup.  Not much new functionality, but massively improved UI

--- js/gui.js
+++ js/gui.js
@@ -1,26 +1,27 @@
 $(function(){
+  var editor = ace.edit("commands");
+  editor.setTheme("ace/theme/github");
+  editor.getSession().setMode("ace/mode/sql");
 
-  // Start the db in which sql.js will run
   var db = new Worker("js/sql.js");
+
   db.onerror = function(e) {
     $('#error').text(e.message).show();
   }
   db.postMessage({action:'open'});
 
-  // Run a command in the database
-  function query(commands, callback) {
+  query = function(commands, callback) {
   	db.onmessage = callback;
   	db.postMessage({action:'exec', sql:commands});
-  }
+  };
 
-  // query the commands when the button is clicked
   $("#execute").click(function() {
     $('#error').hide();
-    query($('#commands').val(), function(event) {
+    query(editor.getValue(), function(event) {
       var results = event.data.results;
       $('#output').empty();
       for (var i=0; i<results.length; i++) {
-        html = '<table><thead><tr><th>' + results[i].columns.join("</th><th>") + '</th></tr></thead><tbody>';
+        html = '<table class="table table-striped"><thead><tr><th>' + results[i].columns.join("</th><th>") + '</th></tr></thead><tbody>';
         for (var j=0; j<results[i].values.length; j++) {
           html += "<tr><td>" + results[i].values[j].join("</td><td>") + "</td></tr>";
         }
@@ -31,16 +32,25 @@ $(function(){
     });
   });
 
-  function refreshTables() {
+  refreshTables = function() {
     query("SELECT name as Tables FROM sqlite_master WHERE type = 'table'; SELECT name as Views FROM sqlite_master WHERE type = 'view';", function(event){
-      var results = event.data.results;
-      $('#tables').html("<a>" + results[0].values.join("</a><br /><a>") + "</a>");
-      $('#views').html("<a>" + results[1].values.join("</a><br /><a>") + "</a>");
+      $('#tables').html("<a>" + event.data.results[0].values.join("</a><br /><a>") + "</a>");
+      $('#views').html("<a>" + event.data.results[1].values.join("</a><br /><a>") + "</a>");
+      $('#tables a, #views a').click(function(){
+        query("SELECT * FROM '" + $(this).text() + "';",function(){
+          var results = event.data.results;
+          html = '<table class="table table-striped"><thead><tr><th>' + results[0].columns.join("</th><th>") + '</th></tr></thead><tbody>';
+          for (var j=0; j<results[0].values.length; j++) {
+            html += "<tr><td>" + results[0].values[j].join("</td><td>") + "</td></tr>";
+          }
+          html += '</tbody></table>'
+          $('#output').html(html);
+        });
+      });
     });
   };
 
-  // Load a db from a file
-  $('#dbfile')[0].onchange = function(){
+  $('#dbfile').change(function(){
   	var f = $('#dbfile')[0].files[0];
   	var r = new FileReader();
   	r.onload = function() {
@@ -52,9 +62,8 @@ $(function(){
   		}
   	}
   	r.readAsArrayBuffer(f);
-  }
+  });
 
-  // Save the db to a file
   $('#savedb').mousedown(function(){
     db.onmessage = function(event) {
       var arraybuff = event.data.buffer;
